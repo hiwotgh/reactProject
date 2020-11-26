989f8f235068452346e51c92460fdf597ccb9c9f
hiwotg <hiwotg734@gmail.com>
2020-11-26 00:08:11 -0500

    first commit

---
+++ src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+const Logo = (props) => {
+  return (
+    <div className="logo">
+      <a href="#">Hiwot Gezahegn</a>
+    </div>
+  );
+};
+export default Logo;
