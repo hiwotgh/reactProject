7e13c75febd20d98fa322a4e0b07300df9c2fa11
hiwotg <hiwotg734@gmail.com>
2020-11-27 14:59:16 -0500

    started working on Post

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import "./style.css";
 
 const Navbar = (props) => {
@@ -16,16 +17,16 @@ const Navbar = (props) => {
     <div className="navbar">
       <ul className="navbarMenu">
         <li>
-          <a href="/#">Home</a>
+          <NavLink to="/#">Home</NavLink>
         </li>
         <li>
-          <a href="/#">About Us</a>
+          <NavLink to="/#">About Us</NavLink>
         </li>
         <li>
-          <a href="/#">Posts</a>
+          <NavLink to="/#">Posts</NavLink>
         </li>
         <li>
-          <a href="/#">Contact Us</a>
+          <NavLink to="/#">Contact Us</NavLink>
         </li>
       </ul>
       <div className="search">
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import "./style.css";
 
 const Home = (props) => {
-  return <div></div>;
+  return <div>Home</div>;
 };
 export default Home;
