9430f4b752f79ec24ca3d3aaf863f5d3eea76e04
hiwotg <hiwotg734@gmail.com>
2020-11-27 11:05:19 -0500

    Fixed bug

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,9 +5,9 @@ const Header = (props) => {
   return (
     <header className="header">
       <nav className="headerMenu">
-        <a href="#">Home</a>
-        <a href="#">About Us</a>
-        <a href="#">Contact Us</a>
+        <a href="/#">Home</a>
+        <a href="/#">About Us</a>
+        <a href="/#">Contact Us</a>
       </nav>
       <div>Social Media Links</div>
     </header>
