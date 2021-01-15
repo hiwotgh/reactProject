31ec4d04f51a3e9c3f010aef3969ee1d1ddd2bfa
hiwotg <hiwotg734@gmail.com>
2021-01-15 16:50:23 -0500

    progress on BlogPost

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -3,8 +3,23 @@ import "./style.css";
 import Card from "../UI/Card";
 
 const BlogPost = (props) => {
-  return;
-  <Card width="70%"></Card>;
+  return (
+    <div className="blogPostContainer">
+      <Card>
+        <div className="blogHeader">
+          <span blogCategory>Featured</span>
+          <h1 className="postTitle">Beautiful is always beautiful</h1>
+          <span className="postedBy">posted on January 15, 2021 by Hiwot </span>
+        </div>
+        <div className="postimageContainer">
+          <img
+            src={require("../../blogPostImages/memories-from.jpg")}
+            alt="Post image"
+          />
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default BlogPost;
--- src/containers/Post/index.js
+++ src/containers/Post/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BlogPost from "../../components/BlogPost";
 import Sidebar from "../../components/Sidebar";
-import Card from "../../components/UI/Card";
+
 import "./style.css";
 
 const Post = (props) => {
